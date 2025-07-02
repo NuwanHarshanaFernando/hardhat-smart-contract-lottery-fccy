c559d67cc50e434b030fbcbd16741ee143ba8eb2
root <root@DESKTOP-932DINA.localdomain>
2025-07-02 17:19:25 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,25 @@
+To start run this command
+```
+yarn add --dev hardhat
+```
+
+Then create an empty hardhat.config.js
+
+```
+yarn hardhat
+```
+You can see empty hardhat.config file
+
+Then install dependencies using the command in the repo...
+
+```
+yarn add --dev @nomiclabs/hardhat-ethers@npm:hardhat-deploy-ethers ethers @nomiclabs/hardhat-etherscan @nomiclabs/hardhat-waffle chai ethereum-waffle hardhat hardhat-contract-sizer hardhat-deploy hardhat-gas-reporter prettier prettier-plugin-solidity solhint solidity-coverage dotenv
+```
+
+Raffle
+
+Enter the lottery (paying some amount)
+Pick a rondom winner (verifiably random)
+Winner to be selected every X minutes -> completely automated
+
+Chainlink Oracle -> Randomness, Automated Execution (Chainlink Keepers)
---
+++ contracts/Raffle.sol
@@ -0,0 +1,41 @@
+// Raffle
+
+// Enter the lottery (paying some amount)
+// Pick a rondom winner (verifiably random)
+// Winner to be selected every X minutes -> completely automated
+
+// Chainlink Oracle -> Randomness, Automated Execution (Chainlink Keepers)
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+error Raffle__NotEnoughETHEntered();
+
+contract Raffle {
+    /* State Variables */
+    uint256 private immutable i_entrance_fee;
+    address payable[] private s_players;
+
+    constructor(uint256 entranceFee) {
+        i_entrance_fee = entranceFee;
+    }
+
+    function enterRaffle() public payable {
+        // require (msg.value > i_entranceFee, "Not enough ETH")
+        if (msg.value < i_entrance_fee) {
+            revert Raffle__NotEnoughETHEntered();
+        }
+        s_players.push(payable(msg.sender));
+    }
+
+    // function pickRandomWinner() {}
+
+    function getEntranceFee() public view returns (uint256) {
+        return i_entrance_fee;
+    }
+
+    function getPlayer(uint256 index) public view returns (address) {
+        return s_players[index];
+    }
+}
